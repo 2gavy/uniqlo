09a77e0b962e50f102e135f8c88a050bde4c00b2
J <j@js-macbook-air.lan>
2019-03-24 17:27:17 +0800

    Added few metafields

--- spiders/uniqlo.py
+++ spiders/uniqlo.py
@@ -23,6 +23,15 @@ class UniqloSpider(scrapy.Spider):
         try:
             name = ''.join(response.css(
                 "#prodInfo > h1 > span ::text").extract())
+            itemcode = ''.join(response.css(
+                "div.basicinfo_wrap > span > ul > li.number::text").extract()).replace("ITEM CODE: ", "")
+            priceCurrency = ''.join(response.css(
+                "div.basicinfo_wrap > span > ul > li:nth-child(2) > div.price-box > meta::attr(content)").extract())
+            price = ''.join(response.css(
+                "div.basicinfo_wrap > span > ul > li:nth-child(2) > div > p.special-price > span.price::text").extract())
+            oldPrice = ''.join(response.css(
+                "div.basicinfo_wrap > span > ul > li:nth-child(2) > div > p.old-price > span.price::text").extract())
+            tags = []
             print(name.strip())
         except Exception as ex:
             print("No name: " + response.url)
